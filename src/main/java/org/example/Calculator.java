af7eb2c9917fde870a72d8a1121f0ad74ec001e4
dai phan phuc <dai582005@gmail.com>
2025-04-14 12:41:01 +0700

    update

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Calculator {
     public Integer sum(Integer a, Integer b) {
-        return a+b;
+        int c = a+b;
+        return c ;
     }
 
     public Integer sub(Integer a, Integer b) {
